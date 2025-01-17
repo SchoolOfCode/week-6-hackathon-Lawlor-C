86b3ebf4121230fc4cd562a33147ab09206c591c
Chris Lawlor <chris.d.lawlor@gmail.com>
2025-01-17 10:56:22 +0000

    updated main to refer to Presentation

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-// THE LOCKED DOOR
-
-
-// Aboard the starship Awkward Mathematics, you find yourself confronted by a locked door with a numerical keypad, 
-// into which the passcode must be entered.
-
-// However, in keeping with the ship's slightly convoluted engineering practices, it has a couple of stipulations which, 
-// once known, may actually be considered something of a security oversight...
-
-
-// The door's requirements are as follows:
-
-// To input the code into the door, it must be first expressed via a function, which takes in two integer inputs 
-// and mutiplies them together, to produce the code itself.
-
-// The resulting code must have TWO digits (no more, no fewer).
-// The code MUST be a perfect square number. (Although the arguments do not both have to be the same number)
-
+//INSTRUCTIONS IN PRESENTATION DOCUMENT
 
 
 //Please run npm install to begin...
